0e1d640b33026c990f220803e476c702b6d14084
mschemmel <mschemmel@online.de>
2023-11-06 23:00:20 +0100

    calculate geometric mean if multiple housekeeping genes are given

--- R/backend.r
+++ R/backend.r
@@ -40,21 +40,26 @@ delta_cq <- function(df) {
 #' @param df data frame of requested groups
 #' @param hkg character vector of housekeeping genes
 #' @examples
-#' ratio_by_mean_ratio(df, d_cq, e_val, hkg_, treatm)
+#' ratio_by_mean_ratio(df, hkg)
 ratio_by_mean_ratio <- function(df, hkg) {
     df <- delta_cq(df)
     target_df <- df[df$gene == setdiff(df$gene, hkg), ]
-    hkg_df <- df[df$gene == hkg, ]
-    target_df$cmp <- (target_df$E^target_df$delta_cq) / (hkg_df$E^hkg_df$delta_cq)
-    target_df$rexpr <- as.numeric(target_df$cmp / mean(get_reference_group(target_df)$cmp))
-    return(drop_columns(target_df, c("control_mean", "delta_cq", "cmp")))
+    target_df$ratio <- target_df$E^target_df$delta_cq
+    hkg_df <- df[df$gene %in% hkg, ]
+    hkg_df$ratio <- hkg_df$E^hkg_df$delta_cq
+
+    ratio <- apply(data.frame(do.call(cbind, lapply(split(hkg_df, hkg_df$gene), function(x) x$ratio))), 1, geometric_mean)
+
+    target_df$cmp_ratio <- (target_df$ratio) / ratio
+    target_df$rexpr <- as.numeric(target_df$cmp_ratio / mean(get_reference_group(target_df)$cmp_ratio))
+    return(drop_columns(target_df, c("control_mean", "delta_cq", "cmp_ratio")))
 }
 
-#' Apply calculation for every hkg vs. target pair
+#' Apply calculation for every HKG vs. target pair
 #' @param pair named list of pairs (data)
 #' @param hkg character vector of housekeeping genes
 #' @examples
-#' pair_wise(pair, e_values, hkg)
+#' pair_wise(pair, hkg)
 pair_wise <- function(pair, hkg) {
     return(lapply(pair, function(x) {
         ratio_by_mean_ratio(x, hkg)
