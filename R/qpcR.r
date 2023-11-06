f3386f3727726106e0d742081d24e6f341c272e5
mschemmel <mschemmel@online.de>
2023-11-06 23:56:15 +0100

    correct function documentation

--- R/qpcR.r
+++ R/qpcR.r
@@ -3,7 +3,8 @@ qenv <- new.env()
 #' Main function to perform calculation of relative expression of qpcr data
 #' @param df data frame of qpcr data
 #' @param hkg character vector of housekeeping genes
-#' @param efficiency named list of efficiency values
+#' @param reference character vector of reference sample
+#' @param groups character vector of conditions to group on
 #' @examples
 #' qpcr(df, hkg = c("HKG"))
 #' @export
