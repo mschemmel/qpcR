8f60250252d3f8b09b47a04aaab35e3ea5a4a4d7
mschemmel <mschemmel@online.de>
2023-11-08 22:44:39 +0100

    add test to control 1 for control groups

--- tests/testthat/test_qpcr.R
+++ tests/testthat/test_qpcr.R
@@ -1,7 +1,10 @@
 test_that("qpcR() works", {
     dat <- read.table("example2.tsv", sep = "\t", head = TRUE)
-    run <- qpcR(dat, hkg = "HKG", reference = "control", groups = "dpi")
-    run <- run[run$treatment == "control", ]
+    run <- get_reference_group(qpcR(dat, hkg = "HKG", reference = "control", groups = "dpi"))
     control_groups <- as.numeric(tapply(run$rexpr, list(run$gene, run$dpi), FUN = mean))
     expect_equal(unique(control_groups), c(1, 1))
+
+    run2 <- get_reference_group(qpcR(dat, hkg = "HKG", reference = "control", groups = "dpi"))
+    control_groups2 <- as.numeric(tapply(run$rexpr, list(run$gene), FUN = mean))
+    expect_equal(unique(control_groups2), c(1, 1))
 })
