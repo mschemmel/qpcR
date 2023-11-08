3e863f4b4f290203138b7e141275cda71e1c0936
mschemmel <mschemmel@online.de>
2023-11-09 00:53:27 +0100

    edit test

--- tests/testthat/test_qpcr.R
+++ tests/testthat/test_qpcr.R
@@ -1,10 +1,10 @@
 test_that("qpcR() works", {
     dat <- read.table("example2.tsv", sep = "\t", head = TRUE)
     run <- get_reference_group(qpcR(dat, hkg = "HKG", reference = "control", groups = "dpi"))
-    control_groups <- as.numeric(tapply(run$rexpr, list(run$gene, run$dpi), FUN = mean))
-    expect_equal(unique(control_groups), c(1, 1))
+    control_groups <- aggregate(rexpr ~ gene + dpi, data = run, FUN = mean)
+    expect_equal(unique(control_groups$rexpr), c(1, 1))
 
-    run2 <- get_reference_group(qpcR(dat, hkg = "HKG", reference = "control", groups = "dpi"))
-    control_groups2 <- as.numeric(tapply(run$rexpr, list(run$gene), FUN = mean))
-    expect_equal(unique(control_groups2), c(1, 1))
+    run2 <- get_reference_group(qpcR(dat, hkg = "HKG", reference = "control"))
+    control_groups2 <- aggregate(rexpr ~ gene, data = run2, FUN = mean)
+    expect_equal(unique(control_groups2$rexpr), c(1, 1))
 })
