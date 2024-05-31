1a805048801e7c95bc4442bebe301a6ee6218d70
mschemmel <mschemmel@online.de>
2024-05-31 23:34:04 +0200

    not needed anymore

--- tests/testthat/test_qpcr.R
+++ tests/testthat/test_qpcr.R
@@ -7,8 +7,4 @@ test_that("qpcR() works", {
     run2 <- get_reference_group(qpcR(dat, hkg = c("HKG", "HKG2"), reference = "control", groups = "dpi", aggregate = FALSE))
     control_groups2 <- aggregate(rexpr ~ gene + dpi, data = run2, FUN = mean)
     expect_equal(unique(control_groups2$rexpr), 1)
-
-    run3 <- get_reference_group(qpcR(dat, hkg = "HKG", reference = "control", aggregate = FALSE))
-    control_groups3 <- aggregate(rexpr ~ gene, data = run3, FUN = mean)
-    expect_equal(unique(control_groups3$rexpr), c(1, 1))
 })
