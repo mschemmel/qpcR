2267320252cd450ed39bef544a5ed28d132b221c
mschemmel <mschemmel@online.de>
2024-06-27 11:53:08 +0200

    [skip ci] add more tests

--- tests/testthat/test_qpcr.R
+++ tests/testthat/test_qpcr.R
@@ -1,4 +1,5 @@
 dat <- read.table("example2.tsv", sep = "\t", head = TRUE)
+
 test_that("qpcR() works", {
     run <- get_reference_group(qpcR(dat, hkg = "HKG", reference = "control", groups = "dpi", aggregate = FALSE))
     control_groups <- aggregate(rexpr ~ gene + dpi, data = run, FUN = mean)
@@ -7,18 +8,28 @@ test_that("qpcR() works", {
     run2 <- get_reference_group(qpcR(dat, hkg = c("HKG", "HKG2"), reference = "control", groups = "dpi", aggregate = FALSE))
     control_groups2 <- aggregate(rexpr ~ gene + dpi, data = run2, FUN = mean)
     expect_equal(unique(control_groups2$rexpr), c(1, 1))
+
+    expect_no_error(qpcR(qpcRdata, hkg = "HKG", groups = "dpi", outlier.method = "interquartile"))
+    expect_no_error(qpcR(qpcRdata, hkg = "HKG", groups = "dpi", outlier.method = "z-score"))
+    expect_no_error(qpcR(qpcRdata, hkg = "HKG", groups = "dpi", outlier.method = "hampel"))
+
+    expect_no_error(qpcR(qpcRdata, hkg = c("HKG", "HKG2"), groups = "dpi", outlier.method = "interquartile"))
+    expect_no_error(qpcR(qpcRdata, hkg = c("HKG", "HKG2"), groups = "dpi", outlier.method = "z-score"))
+    expect_no_error(qpcR(qpcRdata, hkg = c("HKG", "HKG2"), groups = "dpi", outlier.method = "hampel"))
 })
 
 test_that("output equals data.frame", {
-    expect_type(qpcR(dat, hkg = c("HKG"), groups = "dpi"), "list")
+    expect_type(qpcR(dat, hkg = "HKG", groups = "dpi"), "list")
+    expect_type(qpcR(dat, hkg = c("HKG", "HKG2"), groups = "dpi"), "list")
 })
 
 test_that("all output columns present", {
-    expect_equal(colnames(qpcR(dat, hkg = c("HKG"), groups = "dpi")), c("treatment", "gene", "dpi", "rexpr.mean", "rexpr.sd", "rexpr.se", "rexpr.n"))
+    expect_equal(colnames(qpcR(dat, hkg = "HKG", groups = "dpi")), c("treatment", "gene", "dpi", "rexpr.mean", "rexpr.sd", "rexpr.se", "rexpr.n"))
 })
 
 test_that("valid input parameter present", {
     expect_error(qpcR(dat, groups = "dpi"))
-    expect_error(qpcR(dat, hkg = c("HKG"), treatment = "Control", groups = "dpi"))
-    expect_error(qpcR(dat, hkg = c("HKG_"), groups = "dpi"))
+    expect_error(qpcR(dat, hkg = "HKG", treatment = "Control", groups = "dpi"))
+    expect_error(qpcR(dat, hkg = "HKG_", groups = "dpi"))
+    expect_error(qpcR(dat, hkg = "HKG", groups = "dpI"))
 })
